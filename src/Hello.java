77814a4da50c643dbe694df3516e3f81e39bf287
hamed <hamediran1999@gmail.com>
2021-09-13 20:29:28 +0430

    java varibnle

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hi");
+        int age = 22;
+        String x;
+        boolean y;
+        y = true;
+        x = "hamed";
+        System.out.println(age);
+        System.out.println(x);
+        System.out.println(y);
     }
 }
